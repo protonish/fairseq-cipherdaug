05f43faaa70c6757ed64b7f990ed30fa322dad46
Nishant Kambhatla <nkambhat@sfu.ca>
2021-11-08 22:33:28 -0800

    transformer_small

--- fairseq/models/transformer/transformer_legacy.py
+++ fairseq/models/transformer/transformer_legacy.py
@@ -148,6 +148,16 @@ def tiny_architecture(args):
     args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 2)
     return base_architecture(args)
 
+@register_model_architecture("transformer", "transformer_small")
+def tiny_architecture(args):
+    args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 128)
+    args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 256)
+    args.encoder_layers = getattr(args, "encoder_layers", 4)
+    args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 2)
+    args.decoder_layers = getattr(args, "decoder_layers", 4)
+    args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 2)
+    return base_architecture(args)
+
 
 @register_model_architecture("transformer", "transformer")
 def base_architecture(args):
